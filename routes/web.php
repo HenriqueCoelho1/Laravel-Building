66de065ac1656ab375da65620be60b29bda44bfd
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-09-23 15:49:07 -0300

    Controller and Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/about', function () {
+    echo "Hello this is about page in the /admin/about page";
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "This is user number " . $id;
+});
+
+Route::get('/admin/posts/example', array('as' => 'admin.home' ,function () {
+    $url = route('admin.home');
+    return "this is a url " . $url;
+}));
